efdba4f0f83b41adc9bee5ee0ee1adb3b843fc26
Samuel Mayle <117794654+Sampriv@users.noreply.github.com>
2023-11-28 21:59:56 -0500

    Update 2023_sam_7.032 (1).py

--- 2023_sam_7.032 (1).py
+++ 2023_sam_7.032 (1).py
@@ -1,21 +1,29 @@
-from os import name
-
-
 class Flaherty(object):
-    def __init__(self, name, wallet_value, possessions):
+    def __init__(self, name, wallet_value=0, possessions=None):
         self.name = name
         self.wallet_value = wallet_value
-        self.possessions = possessions
+        self.possessions = possessions or []
 
-def earn(wallet_value):
-    wallet_value += 30000
+    def earn(self):
+        self.wallet_value += 30000
     
-def buy(wallet_value, possessions, item, cost):
-    wallet_value -= cost
-    possessions.append(item)
+    def buy(self, item, cost):
+        self.wallet_value -= cost
+        self.possessions.append(item)
     
-def tweet(wallet_value, possessions):
-    string = f"My name is {name} and I have {possessions}"
-    
+    def tweet(self):
+        string = f"My name is {self.name} and I have {self.possessions}"
+        return string
 
-    
+# Create two Flaherty objects
+ned = Flaherty('Ned', 7739330, [])
+juanita = Flaherty('Juanita', 320000, [])
+
+# Call the earn method for the ned object
+ned.earn()
+
+# Call the buy method for the ned object with swapped names and items
+ned.buy('food', 1273330)
+
+# Call the tweet method for the ned object and print the result
+print(ned.tweet())
